1b5ff5d00d3dbc16e9232f82bb2f06ba1e64e248
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-11-27 16:04:15 +0300

    :pencil: Add Media Controls

--- App.js
+++ App.js
@@ -1,45 +1,113 @@
-import React from 'react';
-import {
-    StyleSheet,
-    View,
-} from 'react-native';
-
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import Video from 'react-native-video';
+import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
 
-const App = () => {
-    let player = null;
-
-    const onBuffer = (e) => {
-        console.log('Buffer')
+class App extends Component {
+    videoPlayer;
+    state = {
+        currentTime: 0,
+        duration: 2,
+        isFullScreen: true,
+        isLoading: true,
+        paused: false,
+        playerState: PLAYER_STATES.PLAYING,
     };
 
-    const videoError = (e) => {
-        console.log(e)
+    onSeek = seek => {
+        this.videoPlayer.seek(seek);
     };
 
-    return (
-        <View>
-            <Video source={{uri: "http://178.124.183.14:80/hls/HNBMWPATWZ/variant.m3u8"}}
-                   ref={(ref) => {
-                       player = ref
-                   }}
-                   controls={true}
-                   onBuffer={onBuffer}
-                   onError={videoError}
-                   style={styles.backgroundVideo}
-            />
+    onPaused = playerState => {
+        this.setState({
+            paused: !this.state.paused,
+            playerState,
+        });
+    };
+
+    onReplay = () => {
+        this.setState({ playerState: PLAYER_STATES.PLAYING });
+        this.videoPlayer.seek(0);
+    };
+
+    onProgress = data => {
+        const { isLoading, playerState } = this.state;
+        // Video Player will continue progress even if the video already ended
+        if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+            this.setState({ currentTime: data.currentTime });
+        }
+    };
+
+    onLoad = data => this.setState({ duration: data.duration, isLoading: false });
+
+    onLoadStart = data => this.setState({ isLoading: true });
+
+    onEnd = () => this.setState({ playerState: PLAYER_STATES.ENDED });
+
+    onError = () => alert('Oh! ', error);
+
+    exitFullScreen = () => {};
+
+    enterFullScreen = () => {};
+
+    onFullScreen = () => {};
+
+    renderToolbar = () => (
+        <View style={styles.toolbar}>
+            <Text>I'm a custom toolbar </Text>
         </View>
     );
-};
+    onSeeking = currentTime => this.setState({ currentTime });
+    render() {
+        return (
+            <View style={styles.container}>
+                <Video
+                    onEnd={this.onEnd}
+                    onLoad={this.onLoad}
+                    onLoadStart={this.onLoadStart}
+                    onProgress={this.onProgress}
+                    paused={this.state.paused}
+                    ref={videoPlayer => (this.videoPlayer = videoPlayer)}
+                    //resizeMode="cover"
+                    source={{ uri: 'http://178.124.183.14:80/hls/HNBMWPATWZ/variant.m3u8' }}
+                    style={styles.mediaPlayer}
+                    volume={0.0}
+                />
+                <MediaControls
+                    duration={this.state.duration}
+                    isLoading={this.state.isLoading}
+                    mainColor="orange"
+                    onFullScreen={this.onFullScreen}
+                    onPaused={this.onPaused}
+                    onReplay={this.onReplay}
+                    onSeek={this.onSeek}
+                    onSeeking={this.onSeeking}
+                    playerState={this.state.playerState}
+                    progress={this.state.currentTime}
+                    toolbar={this.renderToolbar()}
+                />
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-    backgroundVideo: {
+    container: {
+        flex: 1,
+    },
+    toolbar: {
+        marginTop: 30,
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 5,
+    },
+    mediaPlayer: {
         position: 'absolute',
-        height: 400,
         top: 0,
         left: 0,
         bottom: 0,
         right: 0,
+        backgroundColor: 'black',
     },
 });
 
