d4b45425999db0f00a75468909b5b497b49d7993
zikwall <andrey.kapitonov.96@gmail.com>
2019-12-01 17:09:41 +0300

    :pencil2: Web View is OK

--- App.js
+++ App.js
@@ -1,97 +1,35 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Video from 'react-native-video';
-import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
 import { WebView } from 'react-native-webview';
+import { StyleSheet, View } from 'react-native';
 
 class App extends Component {
-    videoPlayer;
     state = {
-        currentTime: 0,
-        duration: 0,
-        isFullScreen: false,
-        isLoading: true,
-        paused: false,
-        playerState: PLAYER_STATES.PLAYING,
-        screenType: 'content',
+        selectedIndex: 1,
     };
 
-    onSeek = seek => {
-        //Handler for change in seekbar
-        this.videoPlayer.seek(seek);
+    onSelect = (selectedIndex) => {
+        this.setState({ selectedIndex });
     };
 
-    onPaused = playerState => {
-        //Handler for Video Pause
-        this.setState({
-            paused: !this.state.paused,
-            playerState,
-        });
+    shouldLoadComponent = (index) => {
+        return index === this.state.selectedIndex;
     };
-
-    onReplay = () => {
-        //Handler for Replay
-        this.setState({ playerState: PLAYER_STATES.PLAYING });
-        this.videoPlayer.seek(0);
-    };
-
-    onProgress = data => {
-        const { isLoading, playerState } = this.state;
-        // Video Player will continue progress even if the video already ended
-        if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
-            this.setState({ currentTime: data.currentTime });
-        }
-    };
-
-    onLoad = data => this.setState({ duration: data.duration, isLoading: false });
-
-    onLoadStart = data => this.setState({ isLoading: true });
-
-    onEnd = () => this.setState({ playerState: PLAYER_STATES.ENDED });
-
-    onError = () => alert('Oh! ', error);
-
-    exitFullScreen = () => {
-        alert('Exit full screen');
-    };
-
-    enterFullScreen = () => {};
-
-    onFullScreen = () => {
-        if (this.state.screenType == 'content')
-            this.setState({ screenType: 'cover' });
-        else this.setState({ screenType: 'content' });
-    };
-    renderToolbar = () => (
-        <View>
-            <Text> toolbar </Text>
-        </View>
-    );
-    onSeeking = currentTime => this.setState({ currentTime });
 
     render() {
         return (
-            <WebView source={{ uri: 'https://zikwall.github.io/vk-tv-desktop' }} />
+            <WebView
+                source={{uri: 'http://tv.zikwall.ru/vktv/embed/give?epg=355'}}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+            />
         );
     }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    toolbar: {
-        marginTop: 30,
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 5,
-    },
-    mediaPlayer: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        backgroundColor: 'black',
-    },
 });
+
 export default App;
