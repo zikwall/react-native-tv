812e93de0a98097559db1bcd7b92fa69997510ae
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-03 14:30:17 +0300

    Make Touchable

--- app/components/channel-item/CommonChannelCardItem.js
+++ app/components/channel-item/CommonChannelCardItem.js
@@ -27,7 +27,7 @@ export const TouchableRoundedImage = ({ style, width=80, height=80, ...props }) 
 
 export const CommonChannelCardItem = ({ title, subtitle, image, imageWidth, imageHeight, size, rating }) => {
     return (
-        <View
+        <TouchableOpacity
             style={
                 [styles.channelCard, {height: size, width: size,}]
             }
@@ -41,7 +41,7 @@ export const CommonChannelCardItem = ({ title, subtitle, image, imageWidth, imag
                     {subtitle}
                 </Text>
             </View>
-        </View>
+        </TouchableOpacity>
     )
 };
 
