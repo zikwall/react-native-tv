007ef537e9fac1dacbc23aa57022b07e9764f275
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-06 01:53:13 +0300

    :art: Clean code

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler'
 import AppMain from './App';
 import {name as appName} from './app.json';
-import { appStore } from './app/redux/Store';
-import { Provider } from 'react-redux';
 
 AppRegistry.registerComponent(appName, () => AppMain);
