c019be22d3e2805911cf0a243ccf1c1eca70c893
zikwall <andrey.kapitonov.96@gmail.com>
2020-01-27 21:47:52 +0300

    :art: Advice component: themed

--- app/components/advice/Advice.js
+++ app/components/advice/Advice.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import IconWrap from '../icon/IconWrap';
 import { human } from 'react-native-typography';
+import { useSelector } from 'react-redux';
+import { getAppTheme } from '../../redux/reducers';
 
 const Advice = ({ advice }) => {
+    const theme = useSelector(state => getAppTheme(state));
+
     return (
         <View style={{ paddingBottom: 20, alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
             <IconWrap name={'info'} size={25} style={{ marginLeft: 15 }} />
-            <Text style={[human.caption1, { marginLeft: 15, marginRight: 5, flex: 1, flexWrap: 'wrap' } ]}>
+            <Text style={[human.caption1, { marginLeft: 15, marginRight: 5, flex: 1, flexWrap: 'wrap', color: theme.primaryColor } ]}>
                 { advice.advice }
             </Text>
         </View>
