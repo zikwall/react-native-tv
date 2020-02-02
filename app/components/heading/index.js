42275ebd4977e0d201e9544b2302d2b00830e497
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-02 23:09:16 +0300

    Header component: style review && add description text

--- app/components/heading/index.js
+++ app/components/heading/index.js
@@ -2,26 +2,35 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { View, Text } from 'react-native';
 import IconWrap from '../icon/IconWrap';
+import { human } from 'react-native-typography';
 
-const Heading = ({text, color, icon, iconColor, styles}) => (
-    <View style={[{ padding: 16, flexDirection: 'row'}, styles]}>
+const Heading = ({ text, color, icon, iconColor, styles, description }) => (
+    <View>
+        <View style={[{ padding: 16, flexDirection: 'row'}, styles]}>
+            {
+                icon && <IconWrap name={icon}  size={17} style={{ marginRight: 5 }} />
+            }
+
+            <Text style={{
+                fontWeight: 'bold',
+                color
+            }}>
+                {text}
+            </Text>
+        </View>
         {
-            icon && <IconWrap name={icon}  size={17} style={{ marginRight: 5 }} />
+            description && <Text style={[ human.caption1, { color: color, paddingBottom: 10 } ]}>
+                { description }
+            </Text>
         }
-
-        <Text style={{
-            fontWeight: 'bold',
-            color
-        }}>
-            {text}
-        </Text>
     </View>
 );
 
 Heading.defaultProps = {
     color: '#000',
     iconColor: '#000',
-    styles: {}
+    styles: {},
+    description: undefined
 };
 
 Heading.propTypes = {
