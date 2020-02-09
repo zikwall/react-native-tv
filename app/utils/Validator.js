0f6f922b7095ef31db173001141d0b538ccc6a66
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-09 20:58:08 +0300

    Add url validator

--- app/utils/Validator.js
+++ app/utils/Validator.js
@@ -25,3 +25,13 @@ export const isValidName = (name) => {
 
     return /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g.test(name);
 };
+
+export const isValidURL = (str) => {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+};
