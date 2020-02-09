6b595cddafcef18709ccf66cf7181d23fce32c8a
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-09 11:36:00 +0300

    Rus i18n

--- app/constants/Auth.js
+++ app/constants/Auth.js
@@ -3,7 +3,7 @@ export const IDENTITY_STORAGE_KEY = 'identity';
 
 export const ERROR_INVALID_PASSWORD = {
     'code': 5,
-    'message': 'Password may contain minimum eight characters, at least one uppercase letter, one lowercase letter and one number.',
+    'message': 'Пароль должен содержать не менее восьми символов, как минимум одну заглавную букву, одну строчную букву и одну цифру.',
     'attributes': [
         'password'
     ]
@@ -11,7 +11,7 @@ export const ERROR_INVALID_PASSWORD = {
 
 export const ERROR_INVALID_USERNAME = {
     'code': 4,
-    'message': 'Username may contain next characters minimum five characters, at least uppercase letter, lowercase letter and number.',
+    'message': 'Имя пользователя должен содержать минимум пять символов, как минимум одну заглавнюу букву, строчную букву и цифру.',
     'attributes': [
         'username'
     ]
@@ -19,7 +19,7 @@ export const ERROR_INVALID_USERNAME = {
 
 export const ERROR_INVALID_EMAIL_ADRESS = {
     'code': 2,
-    'message': 'Invalid email adress.',
+    'message': 'Не правильный адрес электронной почты.',
     'attributes': [
         'email'
     ]
@@ -27,7 +27,7 @@ export const ERROR_INVALID_EMAIL_ADRESS = {
 
 export const ERROR_INVALID_NAME = {
     'code': 3,
-    'message': 'Incorrect name. The name may contain letters of the Latin alphabet and a separator in the form of a space, see for yourself [a-zA-Z\',.-]',
+    'message': 'Имя может содержать буквы латинского алфавита и разделитель в виде пробела, смотрите сами [a-zA-Z \', .-]',
     'attributes': [
         'name'
     ]
