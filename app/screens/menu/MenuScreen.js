15d39b417cea612ccd52441878c79e99800faeaf
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-09 11:29:19 +0300

    Remove search panel

--- app/screens/menu/MenuScreen.js
+++ app/screens/menu/MenuScreen.js
@@ -1,29 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-    Dimensions,
     ScrollView,
-    View
 } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import {connect, useSelector} from 'react-redux';
+import { connect } from 'react-redux';
 import {
     MenuItemLine,
     NavigationHeaderRight,
     NavigationHeaderComponent,
     NavigationHeaderLogo,
     ThemedView,
-    SearchBar,
 } from '../../components';
 import { UserHelper, Fake } from '../../utils';
 import MenuUserInfo from './MenuUserInfo';
-import { getAppTheme } from '../../redux/reducers';
-
-const { width, height } = Dimensions.get('window');
 
 const MenuScreen = ({ navigation, user, isAuthenticated }) => {
-    const theme = useSelector(state => getAppTheme(state));
-    const [ cancelVisible, setCancelVisible ] = useState(false);
-    const [ searchText, setSearchText ] = useState('');
 
     const handleSettingsPress = () => {
         navigation.navigate('UserMenuScreen');
@@ -33,39 +24,12 @@ const MenuScreen = ({ navigation, user, isAuthenticated }) => {
         alert('Press Search')
     };
 
-    const handleOnChangeSearchText = (text) => {
-        if (!!text && !cancelVisible) {
-            setCancelVisible(true);
-        }
-
-        setSearchText(text);
-    };
-
     const onMenuPress = (to) => {
         navigation.navigate(to);
     };
 
     return (
         <ThemedView>
-            <View style={{ paddingBottom: 10 }}>
-                <SearchBar
-                    width={width / 2 + 150}
-                    height={ 40 }
-                    placeholder="Глобальный поиск"
-                    fontColor={theme.primaryColor}
-                    iconColor={theme.primaryColor}
-                    cancelIconColor={theme.primaryColor}
-                    backgroundColor={theme.primaryBackgroundColor}
-                    borderColor={theme.primaryColor}
-                    onChangeText={handleOnChangeSearchText}
-                    onPressCancel={() => {
-                        setSearchText('');
-                        setCancelVisible(false);
-                    }}
-                    onPress={() => alert("onPressss")}
-                    cancelVisible={ cancelVisible }
-                />
-            </View>
             <ScrollView>
                 {
                     isAuthenticated && <MenuUserInfo
