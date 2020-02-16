3c5ce6b00e052318138c154467f1bdbbdba81247
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-16 12:07:00 +0300

    Add new help method

--- app/utils/String.js
+++ app/utils/String.js
@@ -7,3 +7,22 @@ export const validURL= (str) => {
         '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
     return !!pattern.test(str);
 };
+
+export const extractHostname = (url) => {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+};
