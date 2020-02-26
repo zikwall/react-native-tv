c0d6aad67f042af6ab664712bf3544f3e2745955
zikwall <andrey.kapitonov.96@gmail.com>
2020-02-26 21:57:39 +0300

    :art: Code clean

--- app/components/ui/BigTag.js
+++ app/components/ui/BigTag.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import { useSelector } from 'react-redux';
 import { getAppTheme } from '../../redux/reducers';
